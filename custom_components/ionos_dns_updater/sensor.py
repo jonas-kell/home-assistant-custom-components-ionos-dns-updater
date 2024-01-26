701e59c690194fea0dca029e31e8cf782a38a97b
Jonas Kell <jonas-kell@web.de>
2024-01-26 15:58:31 +0100

    feat: update?

--- custom_components/ionos_dns_updater/sensor.py
+++ custom_components/ionos_dns_updater/sensor.py
@@ -344,7 +344,9 @@ class IonosDNSUpdater(DNSUpdater):
 
         if local_address_short != dns_address_short:
             # differs, should be updated
-            _LOGGER.info(f"Attempting update of DNS entry on IONOS API")
+            _LOGGER.info(
+                f"Attempting update of DNS entry on IONOS API {local_address_short} -> {dns_address_short}"
+            )
             status, _ = await self.request(
                 f"https://api.hosting.ionos.com/dns/v1/zones/{self._zone_id}/records/{self._record_id}",
                 "PUT",
